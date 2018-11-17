db3cc3a3d67583b0f9a6dd0bb887ed39d420256f
Thilina Manamgoda <maanafunedu@gmail.com>
2018-11-17 15:54:25 +0530

    Ss

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,8 +1,7 @@
 def info(message) {
 
-    wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
-        sh 'echo "INFO: ${message}"'
-        echo '\\u001B[31mI\'m Red\\u001B[0m Now not'
+    ansiColor('xterm') {
+        echo "\\u001B[31mI\'m Red\\u001B[0m Now not"
     }
 
 }
