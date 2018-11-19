e09ae8cf81570268808d5b6dd6485687e2467807
Thilina Manamgoda <maanafunedu@gmail.com>
2018-11-19 13:52:17 +0530

    ss

--- src/org/wso2/hello.groovy
+++ src/org/wso2/hello.groovy
@@ -1,5 +1,9 @@
 package org.wso2
 
-def color(){
-    echo "\\u001B[31mI\'m Red\\u001B[0m Now not"
+class Utilities implements Serializable {
+    def steps
+    Utilities(steps) {this.steps = steps}
+    def color(){
+        echo "\\u001B[31mI\'m Red\\u001B[0m Now not"
+    }
 }
