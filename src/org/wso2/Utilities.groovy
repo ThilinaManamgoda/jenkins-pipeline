e771911fca930c1c96f9e44da9065631688059f5
Thilina Manamgoda <maanafunedu@gmail.com>
2018-11-19 15:45:05 +0530

    s

--- src/org/wso2/Utilities.groovy
+++ src/org/wso2/Utilities.groovy
@@ -2,12 +2,15 @@ package org.wso2
 
 class Utilities implements Serializable {
     def steps
-    Utilities(steps) {this.steps = steps}
+    def envs
+    Utilities(steps) {this.steps = steps
+    this.envs=steps.env
+    }
     def color() {
         steps.ansiColor('xterm') {
             steps.echo "Hello1 \\u001B[31mRed\\u001B[m"
             steps.withCredentials([steps.usernamePassword(credentialsId: 'creds', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-                steps.sh " echo ${steps.env.USER}"
+                steps.sh " echo ${envs.USER}"
             }
         }
 
