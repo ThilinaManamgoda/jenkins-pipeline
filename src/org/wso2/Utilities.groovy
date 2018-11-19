c810227d95a4fd04c716492b8e5711ec79e66e9a
Thilina Manamgoda <maanafunedu@gmail.com>
2018-11-19 14:46:18 +0530

    ss

--- src/org/wso2/Utilities.groovy
+++ src/org/wso2/Utilities.groovy
@@ -5,8 +5,8 @@ class Utilities implements Serializable {
     Utilities(steps) {this.steps = steps}
     def color() {
         steps.ansiColor('xterm') {
-            steps.echo "Hello \\u001B[31mRed\\u001B[m"
+            steps.echo "Hello1 \\u001B[31mRed\\u001B[m"
         }
-        steps.echo "Hello \\u001B[31mRed\\u001B[m"
+
     }
 }
